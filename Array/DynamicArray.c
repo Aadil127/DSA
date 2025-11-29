d4756d12c7b7e833b6c7bd6924353d3276d8a3a0
Aadil127 <aadilsaiyed230405@gmail.com>
2025-11-29 20:13:18 +0530

    Resolves issue first element geting deleted while using insert and some logic change in memory shifting in insert function.

--- Array/DynamicArray.c
+++ Array/DynamicArray.c
@@ -16,10 +16,10 @@ Array *arrayCreate(size_t size, size_t elementSize){
 //Appends the element at the end of an array
 void arrayAppend(Array *a, void *element){
     if(a->size == a->length){
-        a->size *=2;
+        a->size *= 2;
         a->array = realloc(a->array, a->size * a->elementSize);
     }
-    memcpy((char*)a->array + a->length * a->elementSize, element, a->elementSize);
+    memcpy((char*)a->array + (a->length * a->elementSize), element, a->elementSize);
     a->length++;
 }
 
@@ -30,32 +30,43 @@ int arrayEmpty(Array *a){
 
 //Inserts the element at given index in array
 void arrayInsert(Array *a, void *element, int index){
-    if(a->size == a->length){
-        a->size *=2;
+    if(index >= a->size){
+        a->size = index + 1;
         a->array = realloc(a->array, a->size * a->elementSize);
     }
-    // memcpy((char*)a->array + a->length * a->elementSize, element, a->elementSize);
-    // a->length++;
+    if(a->size <= a->length){
+        a->size *= 2;
+        a->array = realloc(a->array, a->size * a->elementSize);
+    }
+    if(a->length == 0){
+        arrayAppend(a, element);
+        return;
+    }
 
     //shift all elements to right by 1 index
+    if(index > a->length){
+        printf("can not insert becaues given is bigger that array's length.");
+        exit(1);
+    }
+    a->length++;
     memmove(
         (char *)a->array + (index + 1) * a->elementSize,
         (char *)a->array + index * a->elementSize,
-        (a->length - index) * a->elementSize
+        (a->length - index - 1) * a->elementSize
     );
 
     //copy element to give index of an array
     memcpy(
-        (char *)a->array + (index) * a->elementSize,
+        (char *)a->array + index * a->elementSize,
         element,
         a->elementSize
     );
-
-    a->length++;
 }
 
+
+// Removes element at given index
 void arrayDeleteElement(Array *a, int index){
-    if(arrayEmpty(a)){
+    if(a->length == 0){
         printf("Error : Array is empty.");
         exit(1);
     }
@@ -63,7 +74,7 @@ void arrayDeleteElement(Array *a, int index){
     memmove(
         (char *)a->array + index * a->elementSize,
         (char *)a->array + (index + 1) * a->elementSize,
-        (a->length - index) * a->elementSize
+        (a->length - index - 1) * a->elementSize
     );
 
     a->length--;
@@ -76,37 +87,26 @@ void arrayRemove(Array *a){
 }
 
 int main(){
-    Array *a = arrayCreate(2, sizeof(int));
     int number = 100;
+    Array *a = arrayCreate(1, sizeof(int));
     for(int i = 0; i < 10; i++){
         arrayInsert(a, &number, 0);
         // arrayAppend(a, &number);
-        arrayAppend(a, &number);
+        printf("Inserted %d at index 0\n", number);
+        // arrayAppend(a, &number);
         number++;
     }
 
+    printf("\nLength : %lu\n\n",a->length);
+
     int *numberptr;
+    numberptr = (int *)a->array;
     for(int i = 0; i < a->length; i++){
-        numberptr = (int *)((char *)a->array + i * a->elementSize);
         printf("Number : %d\n", *numberptr);
+        numberptr++;
     }
-    int *numberptr2 = (int *)(a->array);
-    printf("Number test. : %d\n", *(numberptr2 + a->length - 1));
-    printf("Number test. : %d\n", numberptr2[0]);
 
     arrayRemove(a);
-
-    Array *a1 = arrayCreate(10, sizeof(int));
-    number = 100;
-    for(int i = 0; i < 10; i++){
-        arrayAppend(a1, &number);
-        number += 100;
-    }
-    arrayDeleteElement(a1, 0);
-    numberptr2 = (int *)(a1->array);
-    printf("Number A1: %d\n", *numberptr2);
-
-    arrayRemove(a1);
 
     return 0;
 }
