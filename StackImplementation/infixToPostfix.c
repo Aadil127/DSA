157097f978df99a3e03acb5a30ad28ad73314657
Aadil127 <211808495+Aadil127@users.noreply.github.com>
2026-02-14 13:32:19 +0530

    Added infix to postfix expression conversion function in stack implementation

---
+++ StackImplementation/infixToPostfix.c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"dynamicSizeStack.h"
+
+#define USER_INPUT_BUFFER 100
+
+int isDigit(char ch);
+int getPrecedence(char ch);
+char *infixToPostfix(char *infixExpression);
+
+int main(void){
+    char infixExpression[USER_INPUT_BUFFER];
+
+    //+, _, *, /, ^, '(' and ')' are supported
+    printf("Enter an expression : ");
+
+    fgets(infixExpression, sizeof(infixExpression), stdin);
+
+    printf("Your input : %s", infixExpression);
+    printf("Postfix expression : %s\n", infixToPostfix(infixExpression));
+
+    return 0;
+}
+
+char *infixToPostfix(char *infixExpression){
+    size_t infixExprssionSize = strlen(infixExpression);
+    char *posfixExpression = malloc(sizeof(char) * infixExprssionSize);
+    size_t posfixExpressionIndex = 0;
+    Stack *stack = stackCreate(sizeof(char));
+
+    char topChar;
+    int infixExpOperatorPrecedence;
+    for(size_t infixExpressionIndex = 0; infixExpressionIndex < infixExprssionSize; infixExpressionIndex++){
+
+        //Just to add white space when one number ends and other starts to distinguish between 11+2 -> 11 2 + 
+        if(infixExpressionIndex > 0 && isDigit(infixExpression[infixExpressionIndex - 1]) && !isDigit(infixExpression[infixExpressionIndex])){
+            posfixExpression[posfixExpressionIndex++] = ' ';
+        }
+
+        //To check if given char is digit and directly add it to postfix expression
+        if(isDigit(infixExpression[infixExpressionIndex])){
+            posfixExpression[posfixExpressionIndex] = infixExpression[infixExpressionIndex];
+            posfixExpressionIndex++;
+        }
+        //If ( directly add it to stack
+        else if(infixExpression[infixExpressionIndex] == '('){
+            stackPush(stack, &infixExpression[infixExpressionIndex]);
+        }
+        //If ) pop the stack until we encounter (
+        else if(infixExpression[infixExpressionIndex] == ')'){
+            stackPop(stack, &topChar);
+            while (topChar != '('){
+                posfixExpression[posfixExpressionIndex] = topChar;
+                posfixExpressionIndex++;
+                stackPop(stack, &topChar);
+            }
+        }
+        //Check precendence if top has higher precedence than given operator pop top and push given operator
+        else if((infixExpOperatorPrecedence = getPrecedence(infixExpression[infixExpressionIndex]))){
+            stackPeek(stack, &topChar);
+            while(!stackEmpty(stack) && (getPrecedence(topChar) >= infixExpOperatorPrecedence)){
+                if(infixExpression[infixExpressionIndex] == '^' && topChar == '^') break;
+                posfixExpression[posfixExpressionIndex] = topChar;
+                posfixExpressionIndex++;
+                stackPop(stack, &topChar);
+                stackPeek(stack, &topChar);
+            }
+            stackPush(stack, &infixExpression[infixExpressionIndex]);
+        }
+        // to allow letters eg A + B * C
+        else{
+            if(infixExpression[infixExpressionIndex] != '\n' && infixExpression[infixExpressionIndex] != ' '){
+                posfixExpression[posfixExpressionIndex] = infixExpression[infixExpressionIndex];
+                posfixExpressionIndex++;
+            }
+        }
+
+    }
+    //If arrived at the end of infix expression pop all of the operator from stack one by one and add it to postfix expression until stack becomes empty
+    while(!stackEmpty(stack)){
+        stackPop(stack, &topChar);
+        posfixExpression[posfixExpressionIndex] = topChar;
+        posfixExpressionIndex++;
+    }
+    
+    posfixExpression[posfixExpressionIndex] = '\0';
+    return posfixExpression;
+}
+
+int isDigit(char ch){
+    if (ch >= '0' && ch <= '9') return 1;
+    return 0;
+}
+
+int getPrecedence(char ch){
+    if(ch == '+' || ch == '-') return 1;
+    else if(ch == '*' || ch == '/') return 2;
+    else if(ch == '^') return 3;
+    else return 0;
+}
