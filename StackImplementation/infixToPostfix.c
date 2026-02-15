96e1db7f52e6e99f3df96771e1c2d0972a4679b9
Aadil127 <211808495+Aadil127@users.noreply.github.com>
2026-02-15 13:20:57 +0530

    Added seperate fucntion that accepts infix expression containing letters or single digit and convert it to postfix

--- StackImplementation/infixToPostfix.c
+++ StackImplementation/infixToPostfix.c
@@ -2,12 +2,15 @@
 #include<stdlib.h>
 #include<string.h>
 #include"dynamicSizeStack.h"
+#include"dynamicArray.h"
 
 #define USER_INPUT_BUFFER 100
 
 int isDigit(char ch);
+int isLetter(char ch);
 int getPrecedence(char ch);
 char *infixToPostfix(char *infixExpression, int *expressionRank);
+char *infixToPostfixLetters(char *infixExpression, int *expressionRank);
 
 int main(void){
     char infixExpression[USER_INPUT_BUFFER];
@@ -19,7 +22,9 @@ int main(void){
 
     //if expression rank is 1 it is valid expression
     int expressionRank;
-    printf("Postfix expression : %s\nExpression rank : %d\n", infixToPostfix(infixExpression, &expressionRank), expressionRank);
+    char *exprssionResult;
+    printf("Postfix expression : %s\nExpression rank : %d\n", exprssionResult = infixToPostfixLetters(infixExpression, &expressionRank), expressionRank);
+    free(exprssionResult);
 
     return 0;
 }
@@ -96,8 +101,76 @@ char *infixToPostfix(char *infixExpression, int *expressionRank){
     return posfixExpression;
 }
 
+//Convert infix to postfix expression only for letters and digit
+//Accepts infix expression string and addres of varible to store expression rank leave it NULL if you do not need expression rank
+//Returns postfix expression string, must call free to deallocte memory of this string
+//Example: A+B, A*(B-C), 1/2-7
+char *infixToPostfixLetters(char *infixExpression, int *expressionRank){
+    Array *postfixExpression = arrayCreate(1, sizeof(char));
+    Stack *stack = stackCreate(sizeof(char));
+    char topChar;
+    int infixExpOperatorPrecedence;
+
+    while(*infixExpression!= '\0'){
+        //To check if given char is a digit or a letter and directly add it to postfix expression
+        if(isDigit(*infixExpression) || isLetter(*infixExpression)){
+            arrayAppend(postfixExpression, infixExpression);
+
+            if(expressionRank != NULL) (*expressionRank)++;
+        }
+        //If ( directly add it to stack
+        else if(*infixExpression == '('){
+            arrayAppend(postfixExpression, infixExpression);
+        }
+        //If ) pop the stack until we encounter (
+        else if(*infixExpression == ')'){
+            stackPop(stack, &topChar);
+            while (topChar != '('){
+                arrayAppend(postfixExpression, &topChar);
+                stackPop(stack, &topChar);
+            }
+        }
+        //Check precendence if top has higher precedence than given operator pop top and push given operator
+        else if((infixExpOperatorPrecedence = getPrecedence(*infixExpression))){
+            stackPeek(stack, &topChar);
+
+            if(expressionRank != NULL) (*expressionRank)--;
+
+            while(!stackEmpty(stack) && (getPrecedence(topChar) >= infixExpOperatorPrecedence)){
+                if(*infixExpression == '^' && topChar == '^') break;
+                arrayAppend(postfixExpression, &topChar);
+                stackPop(stack, &topChar);
+                stackPeek(stack, &topChar);
+            }
+            stackPush(stack, infixExpression);
+        }
+        infixExpression++;
+
+    }
+    //If arrived at the end of infix expression pop all of the operator from stack one by one and add it to postfix expression until stack becomes empty
+    while(!stackEmpty(stack)){
+        stackPop(stack, &topChar);
+        arrayAppend(postfixExpression, &topChar);
+    }
+
+    //Converting array to char to string and freeing memory used by dynamic array
+    size_t postfixExpressionLength = arrayLength(postfixExpression);
+    char *postfixExpressionResult = malloc(postfixExpressionLength + 1);
+    memcpy(postfixExpressionResult, arrayElementR(postfixExpression, 0), postfixExpressionLength);
+    postfixExpressionResult[postfixExpressionLength] = '\0';
+    arrayRemove(postfixExpression);
+    stackRemove(stack);
+
+    return postfixExpressionResult;
+}
+
 int isDigit(char ch){
     if (ch >= '0' && ch <= '9') return 1;
+    return 0;
+}
+
+int isLetter(char ch){
+    if (ch >= 'A' && ch <= 'z') return 1;
     return 0;
 }
 
